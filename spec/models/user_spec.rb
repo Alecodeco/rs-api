88da9c8f19397adff1d849dfbe8b50e8770ba494
codeT450 <alecodeco@gmail.com>
2021-08-17 15:07:39 -0300

    improved user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,33 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  it "is invalid when given insufficient attributes" do
-    expect(User.new(id:1, email: 'test@mail.com')).to be_invalid
+RSpec.describe User, :focus, :type => :model do
+  it "is invalid when given less-than-8-characters password" do
+    expect{
+      create(:user, password: "test", password_confirmation: "test")
+    }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
-  it "is invalid when given less-than-8-characters password" do
-    test_user = User.new(id: 1, 
-                         provider: "email", 
-                         email: "test@test.com", 
-                         password: "test", 
-                         uid: "test@test.com", 
-                         gender: "male", 
-                         created_at: "01-01-01", 
-                         updated_at: "01-01-01")
-
-      expect(test_user).to be_invalid
-    end
-
-    it "is valid when given enough attributes" do
-      test_user = User.new(id: 1, 
-                           provider: "email", 
-                           email: "test@test.com", 
-                           password: "testing", 
-                           uid: "test@test.com", 
-                           gender: "male", 
-                           created_at: "01-01-01", 
-                           updated_at: "01-01-01")
-
-      expect(test_user).to be_valid
+    it "is valid when given valid attributes" do
+      expect {
+        create(:user)
+      }.to change { User.count }.by(1)     
     end
 end
